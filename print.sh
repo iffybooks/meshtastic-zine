8ec54c51dedeb999b5b4891cf54c2c478a8104e9
Iffy Books <96699073+iffybooks@users.noreply.github.com>
2024-07-01 14:02:32 -0400

    Fixed a couple typos

--- print.sh
+++ print.sh
@@ -1,4 +1,8 @@
 PDF_PATH="README-book.pdf"
 exiftool $PDF_PATH | grep "Page Count"
 #Cover
-lp -d Brother_MFC_L2700DW_series -o print-quality=4 -o orientation-requested=4 -o InputSlot=tray-2 -o sides=two-sided-long-edge $PDF_PATH
+lp -d Brother_HL_L6200DW_series -o print-quality=4 -o orientation-requested=4 -o InputSlot=tray-2 -o page-ranges=1-2 -o sides=two-sided-long-edge $PDF_PATH
+#Body ## Check
+lp -o page-ranges=3-26 -d Brother_HL_L6200DW_series -o print-quality=4 -o orientation-requested=4 -o InputSlot=tray-1 -o sides=two-sided-long-edge $PDF_PATH
+sleep 0.1
+
